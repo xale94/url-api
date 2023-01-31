69d6002be9897011ccc649164f533c50ecb098c8
fchale <xale1994@gmail.com>
2023-01-31 18:24:13 +0100

    Modify error handling

--- src/http/middleware/errorHandler.ts
+++ src/http/middleware/errorHandler.ts
@@ -1,10 +1,10 @@
 import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 
-export const errorHandler = (err: any, req: Request, res: Response) => {
-  console.log(`[ERROR] : ${err.message}`);
-  return res.status(err.httpStatusCode || StatusCodes.INTERNAL_SERVER_ERROR).json({
-    status: err.statusCode || '500',
-    message: err.message
+export const errorHandler = (error: any, request: Request, response: Response) => {
+  console.log(`[ERROR] : ${error.message}`)
+  return response.status(error.httpStatusCode || StatusCodes.INTERNAL_SERVER_ERROR).json({
+    status: error.statusCode || '500',
+    message: error.message
   });
 };
