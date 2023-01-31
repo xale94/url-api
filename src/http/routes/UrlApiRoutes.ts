defef0412be7254bf971267e96bc4104f31e5167
fchale <xale1994@gmail.com>
2023-01-31 18:25:54 +0100

    Add service call

--- src/http/routes/UrlApiRoutes.ts
+++ src/http/routes/UrlApiRoutes.ts
@@ -1,13 +1,36 @@
-import express, { Request, Response } from 'express';
+import express, { Request, response, Response } from 'express'
 import { services } from '../../services'
+import { validate } from './../middleware/validators/UrlApiValidator'
 
 export const urlApiRoutes = express.Router();
 
-urlApiRoutes.get('/urls', (req: Request, res: Response): void => {
-  const { a, b } = req.body;
-  const service = services.UrlApiService()
-  // res.json({
-  //   success: false,
-  //   message: 'Missing parameters',
-  // });
+urlApiRoutes.get('/urls', [validate], (req: Request, res: Response, next: Function): void => {
+    try{
+        const urls: Array<string>  = req.body.urls
+        const key = req.body.key
+        const service = services.UrlApiService()
+        let aResults: any[] = []
+        const result: { [key: string]: any } = {}
+        service.execute(urls).then((responses)=> {
+            responses.forEach(axiosResponse => {
+                aResults.push(axiosResponse.data)                
+            })
+            aResults = aResults.flat(1)
+
+            for (let index in aResults){
+                const listOfResult = aResults[index]
+                if(listOfResult[key]){
+                    const keyValue = listOfResult[key]
+                    result[keyValue] = aResults[index]
+                }
+            }
+
+            res.json(result);
+        }).catch((reject)=>{
+            throw new Error('There was a problem with the request.')
+        })
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
 });
--- src/services/UrlApiService/urlApiService.ts
+++ src/services/UrlApiService/urlApiService.ts
@@ -1,8 +1,18 @@
+import { AxiosResponse } from "axios";
+import { CurlHelper } from "./../../helpers/CurlHelper";
 
 export const urlApiService = () => {
 
-    const execute = async () => {
-
+    const execute = async (urls: Array<string>) => {
+        let result = new Array<Promise<AxiosResponse<any, any>>>()
+        urls.forEach((url) => {
+            const data = CurlHelper(url)
+            result.push(data)
+        })
+        return await Promise.all(result)
     }
 
+    return {
+        execute
+    }
 }
