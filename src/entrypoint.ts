6ad370b196402f9c8c016a5ecaafb0e9f06acb60
fchale <xale1994@gmail.com>
2023-01-31 18:24:04 +0100

    Change entry point

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -6,18 +6,17 @@ import express,  { Express }   from 'express';
 
 const initialise = async () => {
     const server: Express = express();
-
+    routes.use(errorHandler)
     server.use(
         bodyParser.urlencoded({
             extended: true
         })
-    );
+    )
     server.use(bodyParser.json());
 
-    server.use(errorHandler);
     server.use('/', routes);
-    server.listen(process.env.API_PORT, () =>
-        console.log('The application is initialised on the port %s', process.env.API_PORT)
+    server.listen(process.env.PORT, () =>
+        console.log('The application is initialised on the port %s', process.env.PORT)
     );
 
     return server;
